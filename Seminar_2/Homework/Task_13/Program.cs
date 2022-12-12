ef8462c7e604b6836400446ac92b152941bf06da
Muxammadali <sherikjumayev1202@gmail.com>
2022-12-12 22:58:52 +0500

    Homework - Task_13

---
+++ Seminar_2/Homework/Task_13/Program.cs
@@ -0,0 +1,22 @@
+﻿// Задача 13: Напишите программу, которая выводит третью цифру заданного числа или сообщает, 
+// что третьей цифры нет. Не использовать string[]
+// 645-> 5
+// 78->третьей цифры нет
+// 32679 -> 6
+
+Console.Write("Введите число: ");
+int num = int.Parse(Console.ReadLine());
+
+int index = 1;
+int stepen = 10;
+if (num > 99) {
+ while (num > 999)
+ {
+        num = num / 10;
+    }
+    int otv = num % 10;
+    Console.Write("-> " + otv);
+}
+else {
+    Console.Write("Нет третьего числа");
+}
