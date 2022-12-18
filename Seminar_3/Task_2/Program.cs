04c653a0fbac577999aaeaf547f1ba35ba0736ee
Muxammadali <sherikjumayev1202@gmail.com>
2022-12-18 17:09:33 +0500

    Seminar_3 - Task_18

---
+++ Seminar_3/Task_2/Program.cs
@@ -0,0 +1,26 @@
+﻿
+Console.Write("Введите номер четверти: ");
+int quarterNumber = int.Parse(Console.ReadLine());
+if (quarterNumber > 0 & quarterNumber < 5)
+{
+    if (quarterNumber == 1)
+    {
+        Console.WriteLine("x > 0 и y > 0");
+    }
+    if (quarterNumber == 2)
+    {
+        Console.WriteLine("x < 0 и y > 0");
+    }
+    if (quarterNumber == 3)
+    {
+        Console.WriteLine("x < 0 и y < 0");
+    }
+    if (quarterNumber == 4)
+    {
+        Console.WriteLine("x > 0 и y < 0");
+    }
+}
+else
+{
+    Console.WriteLine("Просьба указать номер четверти (1, 2, 3, 4)");
+}
